fa504fe667ec95c5937323596c33d6dda1d635dd
Abdelatif Hamdi <abdelatifhamdi702@gmail.com>
2023-03-30 04:22:19 +0100

    SEO

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -18,15 +18,21 @@ const Layout = ({ children }) => {
   return (
     <>
       <Head>
-        <title>EcoHyd</title>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <meta name="description" content="EcoHyd" />
-        <meta name="og:title" property="og:title" content="EcoHyd"></meta>
-        <meta name="twitter:card" content="EcoHyd"></meta>
-        <link rel="canonical" href="https://detour.hibootstrap.com"></link>
+        <meta
+          name="description"
+          content="EURL EcoHyd est une entreprise algérienne de disribution rayonnage. Fournitures, Poses & Maintenance de Rayonnages Lourds & Semi Lourds. EURL EcoHyd is an algerian shelving distribution company. Supplies, Installation & Maintenance of Heavy & Semi-Heavy Shelving."
+        />
+        <meta
+          name="keywords"
+          content="Eurl, Ecohyd, Eurl Ecohyd, Entreprise, Company, Algeria, Algérie, DZ, rayonnage, Tout Travaux De Rayonnage, Installation Réseau Incendie, Maintenance Des Sites Industriels, All Shelving Work, Fire Network Installation, Maintenance Of Industrial Sites, All Painting and Plastering Work, Tous Travaux de Peinture et Placoplatre"
+        />
+        <meta name="robots" content="index,follow" />
+        <link rel="canonical" href="https://www.eurl-ecohyd.com" />
+        <title>Eurl Ecohyd</title>
       </Head>
 
       {pathname === '/index-3' ? (
